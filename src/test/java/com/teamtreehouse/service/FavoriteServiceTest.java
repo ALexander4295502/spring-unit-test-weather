983d6328b14c7c80a6c6aafac724ee617d5d3b2f
Zheng <yuanzhengstl@gmail.com>
2018-02-09 18:18:22 -0800

    add service unit test

--- src/test/java/com/teamtreehouse/service/FavoriteServiceTest.java
+++ src/test/java/com/teamtreehouse/service/FavoriteServiceTest.java
@@ -1,55 +1,59 @@
 package com.teamtreehouse.service;
 
-import com.teamtreehouse.dao.FavoriteDao;
-import com.teamtreehouse.domain.Favorite;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import java.util.Arrays;
-import java.util.List;
-
 import static org.hamcrest.Matchers.instanceOf;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.teamtreehouse.dao.FavoriteDao;
+import com.teamtreehouse.domain.Favorite;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
 @RunWith(MockitoJUnitRunner.class)
 public class FavoriteServiceTest {
 
-    @Mock
-    private FavoriteDao dao;
+  @Mock
+  private FavoriteDao dao;
 
-    @InjectMocks
-    private FavoriteService service = new FavoriteServiceImpl();
+  @InjectMocks
+  private FavoriteService service = new FavoriteServiceImpl();
 
-    @Test
-    public void findAll_ShouldReturnTwo() throws Exception {
-        List<Favorite> faves = Arrays.asList(
-            new Favorite(),
-            new Favorite()
-        );
+  @Test
+  public void findAll_ShouldReturnTwo() throws Exception {
+    List<Favorite> favorites = Arrays.asList(
+        new Favorite(),
+        new Favorite()
+    );
 
-        when(dao.findAll()).thenReturn(faves);
+    when(dao.findAll()).thenReturn(favorites);
+    assertEquals(
+        "findAll should return two favorites",
+        2,
+        service.findAll().size()
+    );
+    verify(dao).findAll();
+  }
 
-        assertEquals("findAll should return two favorites",2,service.findAll().size());
-        verify(dao).findAll();
-    }
+  @Test
+  public void findById_ShouldReturnOne() throws Exception {
+    final long FIND_ONE_ID = 1L;
+    when(dao.findOne(FIND_ONE_ID)).thenReturn(new Favorite());
+    assertThat(service.findById(FIND_ONE_ID), instanceOf(Favorite.class));
+    verify(dao).findOne(FIND_ONE_ID);
+  }
 
-    @Test
-    public void findById_ShouldReturnOne() throws Exception {
-        when(dao.findOne(1L)).thenReturn(new Favorite());
-        assertThat(service.findById(1L),instanceOf(Favorite.class));
-        verify(dao).findOne(1L);
-    }
-
-    @Test(expected = FavoriteNotFoundException.class)
-    public void findById_ShouldThrowFavoriteNotFoundException() throws Exception {
-        when(dao.findOne(1L)).thenReturn(null);
-        service.findById(1L);
-        verify(dao).findOne(1L);
-    }
+  @Test(expected = FavoriteNotFoundException.class)
+  public void findById_ShouldThrowFavoriteNotFoundException() throws Exception {
+    final long FIND_ONE_ID = 1L;
+    when(dao.findOne(FIND_ONE_ID)).thenReturn(null);
+    service.findById(FIND_ONE_ID);
+    verify(dao).findOne(FIND_ONE_ID);
+  }
 }
